6f58c12199dc3d738bfe80ebf15498decc591fcd
RupomGg <radwanrupom2001@gmail.com>
2026-08-29 16:40:58 +0600

    fix: update DJANGO_SETTINGS_MODULE to reference drutopay settings

--- drutopay/urls.py
+++ drutopay/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for digipay project.
+URL configuration for drutopay project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/6.1/topics/http/urls/
